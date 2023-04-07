03b6fd06066036f934c2accda0e235a9cac49c8b
mayaban <shubhaban@yahoo.com>
2023-04-07 11:11:37 -0400

    Added random comment

--- src/main/java/com/example/productservice1/ProductService1Application.java
+++ src/main/java/com/example/productservice1/ProductService1Application.java
@@ -3,10 +3,10 @@ package com.example.productservice1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-
 public class ProductService1Application {
 
     public static void main(String[] args) {
--- src/main/java/com/example/productservice1/dto/ProductResponse.java
+++ src/main/java/com/example/productservice1/dto/ProductResponse.java
@@ -12,7 +12,7 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @Builder
 public class ProductResponse {
-
+        //added some random commentss
     private String id;
     private String name;
     private String description;
